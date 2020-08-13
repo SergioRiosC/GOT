df36bc98150aa1206357389e69cc2538ac1074f5
sergio <49257719+SergioRiosC@users.noreply.github.com>
2020-08-12 23:42:57 -0600

    Comando Add

--- src/Diff/diff.js
+++ src/Diff/diff.js
@@ -3,10 +3,13 @@ const{exec}=require('child_process');
 const fs =require('fs');
 const{main} = require('../huffman')
 
-
+const fname='';
 var path=require('path');
-var file1=path.basename('TXT1');
-var file2=path.basename('TXT2');
+var file1='/home/sergio/Documentos/GitHub/GOT/src/Diff/TXT1';
+var Name1=path.basename(file1);
+var file2='/home/sergio/Documentos/GitHub/GOT/src/Diff/TXT2';
+var Name2=path.basename(file2);
+var fileSRV='/home/sergio/Documentos/GitHub/GOT/src/Diff/txtSRV/TXT2';
 var cambios=false;
 var i=1;
 console.log(file1.toString());
@@ -14,8 +17,13 @@ console.log(file2);
 var modificaciones="";
 var nuevosCambios = "";
 
-lineReader.open(file1, function(err,reader1){
-    lineReader.open(file2, function(err,reader2){
+
+function Diff(FileC){
+fs.createReadStream(FileC).pipe(fs.createWriteStream('/home/sergio/Documentos/GitHub/GOT/src/Diff/TXT2'));
+
+
+lineReader.open('/home/sergio/Documentos/GitHub/GOT/src/Diff/TXT1', function(err,reader1){
+    lineReader.open('/home/sergio/Documentos/GitHub/GOT/src/Diff/TXT2', function(err,reader2){
         while(reader1.hasNextLine() ||reader2.hasNextLine()){
             reader1.nextLine(function(err, line1){
                 reader2.nextLine(function(err, line2){
@@ -27,8 +35,8 @@ lineReader.open(file1, function(err,reader1){
                         cambios=true;
                         
                         console.log("Existe una diferencia en la linea "+i+" de los archivos");
-                        console.log("Archivo llamado "+file1+" contiene: "+line1+"\n"+
-                                    "Archivo llamado "+file2+" contiene: "+line2);
+                        console.log("Archivo llamado "+Name1+" contiene: "+line1+"\n"+
+                                    "Archivo llamado "+Name2+" contiene: "+line2);
                         
                         nuevosCambios = nuevosCambios + i + "|" + line1 + "=>" + line2 + ", ";
                         var CambioTXT= i+"|"+line1+"=>"+line2+"\n";
@@ -47,20 +55,23 @@ lineReader.open(file1, function(err,reader1){
         console.log('camb: '+modificaciones);
 
         if(cambios){
+            console.log("camb");
             fs.writeFile(file1, modificaciones, (err)=>{
                 if(err){throw err;}
                 console.log("Actualizado");
             });
-            fs.createReadStream(file2).pipe(fs.createWriteStream('txtSRV/TXT2') );
+            fs.createReadStream(file2).pipe(fs.createWriteStream(fileSRV) );
             
-            const ls =exec('cd txtSRV', function(error, stdout, stderr){
+            const ls =exec('cd Diff/txtSRV', function(error, stdout, stderr){
+                console.log("CD");
                 if(error){
                     console.log(error.stack);
                     console.log('code: '+error.code);
                     console.log('signal: '+error.signal);
                 }
 
-                const md5 =exec('md5sum txtSRV/'+file2+' > CHECkSUM', function(error, stdout, stderr){
+                const md5 =exec('md5sum '+fileSRV+' > CHECkSUM', function(error, stdout, stderr){
+                    console.log("md5");
                     if(error){
                         console.log(error.stack);
                         console.log('code: '+error.code);
@@ -81,5 +92,7 @@ lineReader.open(file1, function(err,reader1){
 
     });    
 });
+}
+module.exports={Diff};
 
 //main("1|hello=>Este, 2|txtx2=>txt2, 3|ajaj=>bajaja");
